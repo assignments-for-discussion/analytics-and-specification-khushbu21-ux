0000fcf93323926127c0789d3088799f21e634fd
khushbu21-ux <71555128+khushbu21-ux@users.noreply.github.com>
2021-12-08 16:51:36 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,10 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  var i = 0, sum = 0, len = numbers.length;
+  while(i < len) {
+    sum = sum + numbers[i++];
+  }
+  return sum / len;
 }
 
 module.exports = {average};

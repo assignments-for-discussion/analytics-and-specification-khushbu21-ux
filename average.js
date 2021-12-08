3a4e9830ed0f7bc00ff60d86ef222f554b0ffbd9
khushbu21-ux <71555128+khushbu21-ux@users.noreply.github.com>
2021-12-08 17:39:08 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,8 +1,32 @@
-function average(numbers) {
-  if(numbers === 0) return 0;
-  else {
-    return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
-  }
+function filterOutliers(numbers) {
+  var values = numbers.concat();
+
+  values.sort((a, b) => a - b);
+
+  var Quartile_1 = values[Math.floor(values.length / 4)];
+  var Quartile_3 = values[Math.ceil(values.length * (3 / 4)) - 1];
+  var interQuartileRange = Quartile_3 - Quartile_1;
+
+  var maxValue = Quartile_3 + interQuartileRange * 1.5;
+  var minValue = Quartile_1 - interQuartileRange * 1.5;
+
+  var filteredValues = values.filter((x) => x <= maxValue && x >= minValue);
+
+  return filteredValues;
 }
 
-module.exports = {average};
+function average(numbers) {
+  if(numbers.length == 0) {
+    return Nan;
+  }
+   
+  const WithoutNan = numbers.filter((num) => !Number.isNaN(num));
+  const WithoutOutliersAndNan = filterOutliers(WithoutNan);
+
+  return (
+    WithoutOutliersAndNan.reduce((p, c) => p + c, 0) / WithoutOutliersAndNan.length;
+
+  );
+}
+
+module.exports = { average };
